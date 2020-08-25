8715b9b0912f82166ff72b8226fa0648f5341ea3
palagpatski <palagpatski@gmail.com>
2020-08-25 14:37:50 +0800

    Fix script error

--- js/bootstrap-ssce.js
+++ js/bootstrap-ssce.js
@@ -5,7 +5,7 @@
     Drupal.behaviors.topSpacing = {
         attach: function(context, settings) {
         	$( window ).resize(function() {
-    			if $('body').hasClass('navbar-is-fixed-top') {
+    			if ($('body').hasClass('navbar-is-fixed-top')) {
     				var marginTop = $('.navbar-fixed-top').outerHeight();
     				$(this).css({
     		            'margin-top': marginTop,
